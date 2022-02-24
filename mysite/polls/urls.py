e16862ec5277be908f302c31a7f56949f62a6fc2
Jack Shaw <jackshaw810@gmail.com>
2022-02-24 07:34:07 +0000

    Revert "Added logging in and out, signup and a home page"
    This reverts commit 2f82f48ca83e551fa7eeb1614ca223fcaa55a75e.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,10 @@
-"""This is used to map between the URLs and views"""
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout, name='logout'),
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('uploadimage', views.upload_image, name='uploadimage'),
+    path('not_authenticated', views.not_authenticated, name='not_authenticated'),
+    path('successful_upload', views.successful_upload, name='auccessful_upload'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,51 +1,50 @@
-"""This is to handle views, a function that takes a web request and returns a web response"""
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.http import HttpResponse
+import datetime
+from .forms import ImagefieldForm
+from .models import Image
+from django.shortcuts import render, redirect
 
-from .forms import LoginForm, SignupForm
+#placeholder function to return location and date taken from metadata
+def get_img_metadata(img):
+	return '1.001 1.002', datetime.datetime.now()
 
+def not_authenticated(request): 
+    return HttpResponse('you must be logged in to upload an image')
 
-def signup(request):
-    """A view for registration of a new user. If the form passes the validators,
-    a new user will be created. By default, the password is both hashed and salted."""
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def upload_image(request): 
+    context = {}
+    #once the request has been made, process it
+    #to test this page without logging in, comment out the next two lines
+    if not request.user.is_authenticated:
+    	return redirect('not_authenticated')
+    if request.method == "POST": 
+        form = ImagefieldForm(request.POST, request.FILES) 
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            User.objects.create_user(username=username,
-                                     password=password)
-            return HttpResponseRedirect('/polls/login/')
-    else:
-        form = SignupForm()
-    return render(request, 'signup.html', {'form': form})
+        	#santize inputs
+            name = form.cleaned_data.get("name")
+            desc = form.cleaned_data.get("description") 
+            img = form.cleaned_data.get("image")
+            gps, date_taken = get_img_metadata(img)
+            #create the table object
+            obj = Image.objects.create( 
+                                 title = name, 
+                                 description = desc, 
+                                 img = img, 
+                                 gps_coordinates = gps, 
+                                 taken_date = date_taken, 
+                                 score = 0
+                                 ) 
+            obj.save() 
+            print(obj)
+            return redirect('successful_upload')  
+    #display the image upload form
+    else: 
+        form = ImagefieldForm()
+        context['form'] = form
+        return render( request, "uploadfile.html", context) 
 
-
-def login(request):
-    """A view for logging in an already registered user."""
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    auth_login(request, user)
-                    return HttpResponseRedirect('/polls/')
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
-
-
-def home(request):
-    """A view for the homepage, used to access other views."""
-    return render(request, 'home.html')
-
-
-def logout(request):
-    """A view to log out the user"""
-    auth_logout(request)
-    return HttpResponseRedirect('/polls/')
+def successful_upload(request): 
+    return HttpResponse('image successfully uploaded')
