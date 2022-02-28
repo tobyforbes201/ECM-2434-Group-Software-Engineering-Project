df3fbd488ab2e46d0230ba8ef2e6ce5b85aadc41
tobyforbes201 <75478849+tobyforbes201@users.noreply.github.com>
2022-02-28 17:55:42 +0000

    Deleted images to show error message

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -28,6 +28,7 @@ class ImageAdmin(admin.ModelAdmin):
         """Overwriting the image deletion method to allow for overwriting the image."""
         obj.title = "DELETED"
         obj.description = "This image has been deleted by an administrator."
+        obj.url = "images/error.jpg"
         obj.save()
 
     image_tag.short_description = 'Image'
