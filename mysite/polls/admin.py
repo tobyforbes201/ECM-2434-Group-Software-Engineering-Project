6ff9085e6b0fd6e7adaaa93a6d023dc399b8d91b
Jack Shaw <73115676+js1294@users.noreply.github.com>
2022-02-28 15:33:38 +0000

    Updated Admin
    Added the ability for admin to edit, delete and add new users. Admin are able to view images and delete an image. When an image is deleted it updates it saying that it has been deleted and why it has been deleted. The super user is able to edit the score of the user, allowing them to "win" the challenge.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,33 @@
+"""This is used to display the models in the django's admin panel."""
 from django.contrib import admin
+from django.utils.html import format_html
 
-from .models import Question, Image
+from .models import Image
 
-admin.site.register(Question)
-admin.site.register(Image)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    """This is used for looking at all the
+     images saved on the database."""
+    fields = ['user', 'title', 'description', 'img', 'image_tag',
+              'gps_coordinates', 'taken_date', 'score']
+    readonly_fields = ['user', 'title', 'description', 'img',
+                       'image_tag', 'gps_coordinates', 'taken_date']
+    actions = ['delete_model']
+
+    def image_tag(self, img):
+        """This is used to create a displayable image."""
+        return format_html('<img src="{url}" width="150" height="150"/>'
+                           .format(url=img.image.url))
+
+    def has_add_permission(self, request, obj=None):
+        """This is used to update add permissions to false."""
+        return False
+
+    def delete_model(self, request, obj):
+        """Overwriting the image deletion method to allow for overwriting the image."""
+        obj.title = "DELETED"
+        obj.description = "This image has been deleted by an administrator."
+        obj.save()
+
+    image_tag.short_description = 'Image'
