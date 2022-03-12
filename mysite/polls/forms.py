a1a01f19b9c09bff53c9b1dff7b0805462388a51
Jack Shaw <73115676+js1294@users.noreply.github.com>
2022-03-12 23:38:51 +0000

    Pylint fixes
    Added improvements to code readablity and layout using pylint.

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -2,10 +2,11 @@
 from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 
 from .validate import check_user_unique, validate_upper_lower, \
     validate_special, validate_number, check_image_type
+from django.core.exceptions import ValidationError
+
 
 
 class ImagefieldForm(forms.Form):
