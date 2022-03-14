d8c81d08b3cd6dc4a0f95a7704220f468826a34a
tobyforbes201 <75478849+tobyforbes201@users.noreply.github.com>
2022-03-14 18:04:21 +0000

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='polls/')
+    path('', RedirectView.as_view(url='polls/'),
     path('polls/', include('polls.urls')),
     path('polls/uploadimage', include('polls.urls')),
     path('admin/', admin.site.urls),
