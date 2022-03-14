20e9a34cd0d65431ce19d9f24c9fa8b574fa1ace
tobyforbes201 <75478849+tobyforbes201@users.noreply.github.com>
2022-03-14 16:20:56 +0000

    /

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('polls/uploadimage', include('polls.urls')),
+    path('', include('polls.urls')),
+    path('uploadimage', include('polls.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- mysite/tests.py
+++
@@ -1,4 +0,0 @@
-"""Django tests to ensure that the app is working correctly are written and run here."""
-from django.test import TestCase
-
-# Create your tests here.
