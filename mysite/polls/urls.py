c103d65e57ad42617424f6cd7a6d8686bb3f5226
tobyforbes201 <75478849+tobyforbes201@users.noreply.github.com>
2022-03-18 10:27:15 +0000

    compatibility changes
    changes updated to conform with main branch

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -14,7 +14,13 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('feed', views.display_feed, name='feed'),
     path('leaderboards', views.leaderboards, name='leaderboards'),
+    path('profile', views.profile, name='profile'),
     path('', views.home, name='home'),
+    path('viewprofile/<username>', views.view_profile, name='viewprofile'),
+    path('vote/<photo_id>', views.vote, name='vote'),
+    path('unvote/<photo_id>', views.unvote, name='unvote'),
+    path('deletephoto/<photo_id>',views.delete_photo,name='deletephoto'),
+    path('deleteuser/<username>',views.delete_account,name='deleteuser'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 #set the media roots
